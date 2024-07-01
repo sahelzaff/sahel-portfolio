eea946f64a49dad98f40db0c74a9d70fc5e54988
sahelzaff <sahelzaffar2@gmail.com>
2024-07-02 01:36:14 +0530

    contact us issue

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -70,7 +70,7 @@ const Contact = () => {
                 <div className='flex flex-row items-start justify-start bg-[#f8d08d] py-20 px-20' id='contact_main'>
                     <div className='w-1/2 flex flex-col items-start justify-start' id='contact_div1'>
                         <div className='flex flex-col items-start justify-start'>
-                            <h1 className='text-[#0d0d0d] font-outfit text-[45px] font-bold leading-[1]' id='contact_heading'  data-aos='fade-up' data-aos-duration="1000" data-aos-once="true">
+                            <h1 className='text-[#0d0d0d] font-outfit text-[45px] font-bold leading-[1] whitespace-nowrap' id='contact_heading'  data-aos='fade-up' data-aos-duration="1000" data-aos-once="true">
                                 Get in <span className='text-[#efefef]'>Touch</span>
                             </h1>
                             <div className='w-5/12 bg-[#0d0d0d] h-[4px] rounded-lg mt-1 ml-1' data-aos='fade-up' data-aos-duration="1000" data-aos-once="true"></div>
@@ -84,7 +84,7 @@ const Contact = () => {
                         <p className='font-poppins text-[16px] text-gray-800 font-medium mt-8 pr-14' id='contact_para'  data-aos='fade-up' data-aos-duration="1000" data-aos-once="true">
                             I am passionate about web development, and I'm always open to new opportunities and collaborations. Let’s create something amazing together!
                         </p>
-                        <div className='py-20 flex flex-row items-center gap-3'>
+                        <div className='py-20 flex flex-row items-center gap-3'id='quote_main'>
                             <div className='h-20 w-[8px] bg-white' data-aos='fade-right' data-aos-duration="1000" data-aos-once="true"></div>
                             <p className='font-poppins text-[26px] text-black font-bold pr-14' id='contact_quote'  data-aos='fade-left' data-aos-duration="1000" data-aos-once="true"  >
                                 Let's turn your ideas into reality—reach out and let's get started!
