12678bca0d956a734ad9be3edaa6d3538a72ec65
sahelzaff <sahelzaffar2@gmail.com>
2024-07-02 01:21:59 +0530

    overflow hidden

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Footer from './Components/Footer';
 const App = () => {
   return (
     <Router>
-      <div>
+      <div className='overflow-x-hidden'>
         <Navbar />
         <Hero />
         <About />
